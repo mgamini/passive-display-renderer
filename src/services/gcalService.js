846fb9ea7bd29b1148267934f704949f1439794e
Garrett Amini <kadjar@gmail.com>
2022-11-07 18:19:39 -0500

    gcal refactor

--- src/services/gcalService.js
+++ src/services/gcalService.js
@@ -40,45 +40,17 @@ const fetchEvents = async (calendarId, endDayIdx, maxResults) => {
 
   const { data } = await calendar.events.list(params);
 
-  return data.items.map(({ summary, start, end }) => {
-    const allDayEvent = !Object.hasOwn(start, "dateTime");
-
-    return {
-      calendarName: data.summary.replaceAll("(personal)", "").trim(),
-      eventName: summary,
-      allDayEvent,
-      start: new Date(allDayEvent ? `${start.date}T00:00` : start.dateTime),
-      end: new Date(allDayEvent ? `${end.date}T00:00` : end.dateTime),
-    };
-  });
+  return data;
 };
 
 gcalService.getEvents = async (dayCount = 1, maxResults = null) => {
-  const events = [];
   const calendarIds = GOOGLE_CALENDAR_IDS.split(",");
 
-  const eventList = await calendarIds.reduce(async (agg, calendarId) => {
-    const current = await agg;
-    const fetchedEvents = await fetchEvents(
-      calendarId,
-      dayCount - 1,
-      maxResults
-    );
-    return [...current, ...fetchedEvents];
-  }, []);
-
-  for (let i = 0; i < dayCount; i++) {
-    const dayStart = time.getDayStart(i);
-    const dayEnd = time.getDayEnd(i);
-
-    events.push(
-      eventList
-        .filter(({ start }) => start >= dayStart && start < dayEnd)
-        .sort((a, b) => (a.start > b.start ? 1 : -1))
-    );
-  }
-
-  return events;
+  return Promise.all(
+    calendarIds.map(async (calendarId) => {
+      return await fetchEvents(calendarId, dayCount - 1, maxResults);
+    })
+  );
 };
 
 module.exports = gcalService;
