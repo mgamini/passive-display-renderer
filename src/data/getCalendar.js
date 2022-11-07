9ef210ddcd32570fa3a46d317aa6b22172e9dac2
Garrett Amini <kadjar@gmail.com>
2022-11-07 18:19:39 -0500

    gcal refactor

--- src/data/getCalendar.js
+++ src/data/getCalendar.js
@@ -3,27 +3,78 @@ const { time } = require("../util");
 
 const DAY_COUNT = 4;
 
+class Event {
+  constructor(calendarName, { summary, start, end }) {
+    this.calendarName = calendarName.replaceAll("(personal)", "").trim();
+    this.eventName = summary;
+
+    this.allDayEvent = !Object.hasOwn(start, "dateTime");
+    this.start = new Date(
+      this.allDayEvent ? `${start.date}T00:00` : start.dateTime
+    );
+    this.end = new Date(this.allDayEvent ? `${end.date}T00:00` : end.dateTime);
+
+    this.timeLabel = this.allDayEvent
+      ? "All day"
+      : `${time.print.time.hr12(this.start)} - ${time.print.time.hr12(
+          this.end
+        )}`;
+  }
+}
+
 module.exports = async () => {
-  const data = await gcalService.getEvents(DAY_COUNT);
+  const output = [];
+  const fetchedEvents = await gcalService.getEvents(DAY_COUNT);
 
-  return data.map((calEvents, idx) => {
+  console.log(fetchedEvents);
+
+  const eventList = fetchedEvents.reduce((agg, { summary, items }) => {
+    items.forEach((item) => {
+      agg.push(new Event(summary, item));
+    });
+
+    return agg;
+  }, []);
+
+  for (let i = 0; i < DAY_COUNT; i++) {
+    const dayStart = time.getDayStart(i);
+    const dayEnd = time.getDayEnd(i);
+
     const label =
-      idx === 0
-        ? time.print.date.long(time.getDayStart(idx))
-        : time.print.date.short(time.getDayStart(idx));
+      i === 0
+        ? time.print.date.long(time.getDayStart(i))
+        : time.print.date.short(time.getDayStart(i));
 
-    const events = calEvents.map((event) => ({
-      ...event,
-      timeLabel: event.allDayEvent
-        ? "All day"
-        : `${time.print.time.hr12(event.start)} - ${time.print.time.hr12(
-            event.end
-          )}`,
-    }));
+    const events = eventList
+      .filter(({ start }) => start >= dayStart && start < dayEnd)
+      .sort((a, b) => (a.start > b.start ? 1 : -1));
 
-    return {
+    output.push({
       label,
       events,
-    };
-  });
+    });
+  }
+
+  return output;
+
+  // return data.map((calEvents, idx) => {
+  //   const label =
+  //     idx === 0
+  //       ? time.print.date.long(time.getDayStart(idx))
+  //       : time.print.date.short(time.getDayStart(idx));
+
+  //   const events = calEvents.map((event) => ({
+  //     ...event,
+  //     timeLabel: event.allDayEvent
+  //       ? "All day"
+  //       : `${time.print.time.hr12(event.start)} - ${time.print.time.hr12(
+  //           event.end
+  //         )}`,
+  //   }));
+
+  //   return {
+  //     label,
+  //     events,
+  //   };
+  // });
 };
